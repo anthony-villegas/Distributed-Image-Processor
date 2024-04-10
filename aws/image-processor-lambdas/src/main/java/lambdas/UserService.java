030850e4fc86a5976f50d6ee2effb5c525bf4826
Anthony <anthonyvillegas@berkeley.edu>
2024-04-10 15:16:37 +0800

    succesfully connecting to database using HikariCP datasource passed into Jdbi. Exception thrown since I setup schema on each call, should probably have a function that checks if schema setup and sets up if not

--- aws/image-processor-lambdas/src/main/java/lambdas/UserService.java
+++ aws/image-processor-lambdas/src/main/java/lambdas/UserService.java
@@ -2,34 +2,32 @@ package lambdas;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import helpers.DatabaseCredentialsManager;
 import org.jdbi.v3.core.statement.Update;
 import org.jdbi.v3.core.Jdbi;
 
 import schemas.User;
 
-import javax.sql.DataSource;
 import java.util.List;
 
 public class UserService implements RequestHandler<User, List<User>>{
-    private final String dbUrl = System.getenv("DB_ENDPOINT_ADDRESS");
-    private final String secretName = System.getenv("DB_SECRET_NAME");
-
-    private final DatabaseCredentialsManager credentialsManager = new DatabaseCredentialsManager(secretName);
+    private static final String secretName = System.getenv("DB_SECRET_NAME");
+    private static final HikariDataSource dataSource = initializeHikari(
+            new DatabaseCredentialsManager(secretName).getSecretCredentials(),
+            System.getenv("DB_ENDPOINT_ADDRESS"),
+            System.getenv("DB_NAME")
+    );
+    private static final Jdbi jdbi = Jdbi.create(dataSource);
 
     public List<User> handleRequest(User input, Context context) {
-
-        // Retrieve database credentials from AWS Secrets Manager
-        DatabaseCredentialsManager.Credentials credentials = credentialsManager.getSecretCredentials();
-
-        Jdbi jdbi = Jdbi.create(dbUrl, credentials.username(), credentials.password());
-
         context.getLogger().log("Received username: " + input.getUsername());
-        context.getLogger().log("Received password: " + input.getPassword());
+        context.getLogger().log("\nReceived password: " + input.getPassword());
 
         // Create table and insert inputted value
         jdbi.useHandle(handle -> {
-            handle.execute("CREATE TABLE users (username VARCHAR(50), password VARCHAR(50))");
+            handle.execute("CREATE TABLE user (username VARCHAR(50), password VARCHAR(50))");
             Update update = handle.createUpdate("INSERT INTO user (username, password) VALUES (:username, :password)");
             update.bind("username", input.getUsername());
             update.bind("password", input.getPassword());
@@ -44,5 +42,17 @@ public class UserService implements RequestHandler<User, List<User>>{
         });
 
         return users;
+    }
+
+    private static HikariDataSource initializeHikari(DatabaseCredentialsManager.Credentials credentials, String dbEndpoint, String dbName) {
+        // Initialize HikariDataSource
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://" + dbEndpoint + ":3306/" + dbName);
+        config.setUsername(credentials.username());
+        config.setPassword(credentials.password());
+        return new HikariDataSource(config);
+    }
+    public void cleanup() {
+        dataSource.close(); // Close the HikariDataSource
     }
 }
