3378c571151275dc7f1646f4b3003286f2f9626b
Anthony <anthonyvillegas@berkeley.edu>
2024-04-14 16:19:31 +0800

    Creation and deletion of users now working with manual testing. Just need to write tests

--- aws/image-processor-lambdas/src/main/java/lambdas/UserService.java
+++ aws/image-processor-lambdas/src/main/java/lambdas/UserService.java
@@ -2,17 +2,20 @@ package lambdas;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import helpers.DatabaseCredentialsManager;
 import org.jdbi.v3.core.statement.Update;
 import org.jdbi.v3.core.Jdbi;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import schemas.User;
 
-import java.util.List;
+import java.util.Collections;
 
-public class UserService implements RequestHandler<User, List<User>>{
+public class UserService implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent>{
     private static final String secretName = System.getenv("DB_SECRET_NAME");
     private static final HikariDataSource dataSource = initializeHikari(
             new DatabaseCredentialsManager(secretName).getSecretCredentials(),
@@ -20,27 +23,73 @@ public class UserService implements RequestHandler<User, List<User>>{
             System.getenv("DB_NAME")
     );
     private static final Jdbi jdbi = Jdbi.create(dataSource);
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
-    public List<User> handleRequest(User input, Context context) {
-        context.getLogger().log("Creating user: " + input.getUsername());
-        generateSchema();
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context){
+        User user;
 
-        // Insert inputted value
-        jdbi.useHandle(handle -> {
-            Update update = handle.createUpdate("INSERT INTO user (Username, Password) VALUES (:username, :password)");
-            update.bind("username", input.getUsername());
-            update.bind("password", input.getPassword());
-            update.execute();
-        });
+        try {
+             user = objectMapper.readValue(request.getBody(), User.class);
+        } catch (Exception e) {
+            return createErrorResponse(400, "Error deserializing user: " + e.getMessage());
+        }
 
-        // Query table for inputted value
-        List<User> users = jdbi.withHandle(handle -> {
-            return handle.createQuery("SELECT * FROM user")
-                    .mapToBean(User.class)
-                    .list();
-        });
+        switch (request.getHttpMethod()) {
+            case "POST":
+                return createUser(user, context);
+            case "DELETE":
+                return deleteUser(user, context);
+            default:
+                return createErrorResponse(400, "Method not allowed");
+        }
+    }
 
-        return users;
+    private APIGatewayProxyResponseEvent createUser(User user, Context context) {
+        try {
+            context.getLogger().log("Creating user: " + user.getUsername());
+            generateSchema();
+            jdbi.useHandle(handle -> {
+                Update update = handle.createUpdate("INSERT INTO user (Username, Password) VALUES (:username, :password)");
+                update.bind("username", user.getUsername());
+                update.bind("password", user.getPassword());
+                update.execute();
+            });
+            return createSuccessResponse(201, "User created successfully");
+        } catch(Exception e) {
+            return createErrorResponse(500, "Error processing POST request: " + e.getMessage());
+        }
+    }
+
+    private APIGatewayProxyResponseEvent deleteUser(User user, Context context) {
+        try {
+            context.getLogger().log("Deleting user: " + user.getUsername());
+            int rowsAffected = jdbi.withHandle(handle ->
+                handle.createUpdate("DELETE FROM user WHERE username = :username AND password = :password")
+                        .bind("username", user.getUsername())
+                        .bind("password", user.getPassword())
+                        .execute()
+            );
+            if (rowsAffected == 0) {
+                return createErrorResponse(404, "User for deletion not found");
+            }
+            return createSuccessResponse(200, "User deleted successfully");
+        } catch(Exception e) {
+            return createErrorResponse(500, "Error processing DELETE request: " + e.getMessage());
+        }
+    }
+
+    private APIGatewayProxyResponseEvent createSuccessResponse(int statusCode, String message) {
+        return new APIGatewayProxyResponseEvent()
+                .withStatusCode(statusCode)
+                .withHeaders(Collections.singletonMap("Content-Type", "text/plain"))
+                .withBody(message);
+    }
+
+    private APIGatewayProxyResponseEvent createErrorResponse(int statusCode, String errorMessage) {
+        return new APIGatewayProxyResponseEvent()
+                .withStatusCode(statusCode)
+                .withHeaders(Collections.singletonMap("Content-Type", "text/plain"))
+                .withBody(errorMessage);
     }
 
     private static HikariDataSource initializeHikari(DatabaseCredentialsManager.Credentials credentials, String dbEndpoint, String dbName) {
