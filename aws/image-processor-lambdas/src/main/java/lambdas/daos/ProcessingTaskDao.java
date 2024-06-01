1014fc9a675b6e7361c913a89d6d992de0930eaa
anthony-villegas <55756541+anthony-villegas@users.noreply.github.com>
2024-06-01 23:48:56 +0800

    Processing task dao (#9)
    * adding dao for processing task
    
    * forgot to commit unit tests :)
    
    ---------
    
    Co-authored-by: Anthony <anthonyvillegas@berkeley.edu>

---
+++ aws/image-processor-lambdas/src/main/java/lambdas/daos/ProcessingTaskDao.java
@@ -0,0 +1,52 @@
+package lambdas.daos;
+
+import lambdas.beans.ProcessingTaskBean;
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.customizer.BindBean;
+import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
+import java.util.List;
+
+public interface ProcessingTaskDao {
+    @SqlUpdate("CREATE TABLE IF NOT EXISTS processing_task (" +
+            "id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, " +
+            "imageId VARCHAR(100), " +
+            "status ENUM('queued', 'processing', 'timeout', 'failed', 'completed'), " +
+            "retries TINYINT UNSIGNED, " +
+            "result VARCHAR(100), " +
+            "queuedAt TIMESTAMP, " +
+            "startedAt TIMESTAMP, " +
+            "finishedAt TIMESTAMP, " +
+            "FOREIGN KEY (imageId) REFERENCES source_image(id) " +
+            "ON DELETE CASCADE " +
+            "ON UPDATE CASCADE" +
+            ");")
+    void createTable();
+
+    @SqlUpdate("INSERT INTO processing_task (imageId, status, retries, result, queuedAt, startedAt, finishedAt) " +
+            "VALUES (:imageId, :status, :retries, :result, :queuedAt, :startedAt, :finishedAt)")
+    @GetGeneratedKeys("id")
+    int insert(@BindBean ProcessingTaskBean task);
+
+    @SqlUpdate("UPDATE processing_task SET " +
+            "status = :status, " +
+            "retries = :retries, " +
+            "result = :result, " +
+            "queuedAt = :queuedAt, " +
+            "startedAt = :startedAt, " +
+            "finishedAt = :finishedAt " +
+            "WHERE id = :id")
+    void update(@BindBean ProcessingTaskBean task);
+
+    @SqlQuery("SELECT * FROM processing_task WHERE id = :id")
+    @RegisterBeanMapper(ProcessingTaskBean.class)
+    ProcessingTaskBean getProcessingTask(@Bind("id") int id);
+
+    @SqlQuery("SELECT * FROM processing_task WHERE imageId = :imageId ORDER BY id")
+    @RegisterBeanMapper(ProcessingTaskBean.class)
+    List<ProcessingTaskBean> listProcessingTasksByImageId(@Bind("imageId") String imageId);
+}
+
